27fcefc56b36f2a7501bae9adda33a7554fc1f86
Elijah Sink <theminorprofit@gmail.com>
2022-06-19 21:54:10 -0500

    Add support for configuring counts, better result printing, etc

--- word_count/src/counter.rs
+++ word_count/src/counter.rs
@@ -1,21 +1,70 @@
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::{BufRead, BufReader, ErrorKind, Result};
 
-#[derive(Debug)]
+static FIELD_ORDER: [&str; 4] = ["lines", "words", "chars", "bytes"];
+
+#[derive(Debug, Clone)]
+pub struct CounterSettings {
+    count_bytes: bool,
+    count_chars: bool,
+    count_words: bool,
+    count_lines: bool,
+    counted_fields: Box<Vec<String>>,
+}
+
+impl CounterSettings {
+    pub fn new(count_bytes: bool, count_chars: bool, count_words: bool, count_lines: bool) -> Self {
+        let fields = [count_lines, count_words, count_chars, count_bytes];
+        let counted_fields: Vec<String> = fields
+            .iter()
+            .zip(FIELD_ORDER.iter())
+            .filter(|(counted, _)| **counted)
+            .map(|(_, name)| String::from(*name))
+            .collect();
+
+        let counted_fields = Box::new(counted_fields);
+
+        CounterSettings {
+            count_bytes,
+            count_chars,
+            count_words,
+            count_lines,
+            counted_fields,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct Counter {
-    bytes: u64,
-    chars: u64,
-    words: u64,
+    settings: CounterSettings,
     lines: u64,
+    words: u64,
+    chars: u64,
+    bytes: u64,
+    name: String,
+    err: Option<String>,
 }
 
 impl Counter {
-    pub fn new() -> Self {
+    pub fn new(name: String, settings: CounterSettings) -> Self {
         Counter {
+            settings,
             bytes: 0,
             chars: 0,
             words: 0,
             lines: 0,
+            name,
+            err: None,
+        }
+    }
+
+    pub fn index(&self, key: &str) -> u64 {
+        match key {
+            "bytes" => self.bytes,
+            "chars" => self.chars,
+            "words" => self.words,
+            "lines" => self.lines,
+            _ => panic!("Unexpected key: {}", key),
         }
     }
 
@@ -27,10 +76,18 @@ impl Counter {
                     return;
                 }
                 Ok(bytes_read) => {
-                    self.bytes += bytes_read as u64;
-                    self.chars += buff.chars().count() as u64;
-                    self.words += buff.split_ascii_whitespace().count() as u64;
-                    self.lines += 1;
+                    if self.settings.count_bytes {
+                        self.bytes += bytes_read as u64;
+                    }
+                    if self.settings.count_chars {
+                        self.chars += buff.chars().count() as u64;
+                    }
+                    if self.settings.count_words {
+                        self.words += buff.split_ascii_whitespace().count() as u64;
+                    }
+                    if self.settings.count_lines {
+                        self.lines += 1;
+                    }
                 }
                 Err(e) if e.kind() == ErrorKind::Interrupted => {
                     continue;
@@ -48,26 +105,6 @@ impl Counter {
     }
 }
 
-impl std::fmt::Display for Counter {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        let width = [self.lines, self.words, self.chars, self.bytes]
-            .map(|s| s.to_string().len())
-            .iter()
-            .max()
-            .unwrap()
-            .clone();
-        write!(
-            f,
-            "{:width$} {:width$} {:width$} {:width$}",
-            self.lines,
-            self.words,
-            self.chars,
-            self.bytes,
-            width = width
-        )
-    }
-}
-
 #[derive(Debug)]
 pub struct FileCounter {
     pub counts: Counter,
@@ -75,23 +112,65 @@ pub struct FileCounter {
 }
 
 impl FileCounter {
-    pub fn new(filepath: String) -> Self {
+    pub fn new(filepath: String, settings: CounterSettings) -> Self {
         FileCounter {
-            counts: Counter::new(),
+            counts: Counter::new(filepath.clone(), settings),
             filepath,
         }
     }
 
     pub fn count(&mut self) -> Result<()> {
-        let f = File::open(self.filepath.as_str())?;
-        let reader = BufReader::new(f);
-        self.counts.count_stream(reader);
+        let metadata = fs::metadata(self.filepath.as_str())?;
+        let filetype = metadata.file_type();
+        if filetype.is_dir() {
+            self.counts.err = Some(format!("wc: {}: is a directory", self.filepath))
+        } else {
+            let f = File::open(self.filepath.as_str())?;
+            let reader = BufReader::new(f);
+            self.counts.count_stream(reader);
+        }
         Ok(())
     }
 }
 
-impl std::fmt::Display for FileCounter {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{} {}", self.counts, self.filepath)
+pub fn display_counters(counters: &mut Vec<Counter>, settings: CounterSettings) {
+    let mut totals = Counter::new(String::from("total"), settings.clone());
+    let mut all_counts_for_width: Vec<u64> = Vec::new();
+
+    for counter in counters.iter() {
+        if counter.err.is_some() {
+            continue;
+        }
+        totals.add(&counter);
+        for counted_field in settings.counted_fields.iter() {
+            all_counts_for_width.push(counter.index(counted_field));
+        }
+    }
+
+    if counters.len() > 1 {
+        counters.push(totals.clone());
+        all_counts_for_width.extend(
+            settings
+                .counted_fields
+                .iter()
+                .map(|name| totals.index(name)),
+        );
+    }
+
+    let width = all_counts_for_width
+        .iter()
+        .map(|s| s.to_string().len())
+        .max()
+        .unwrap()
+        .clone();
+
+    for counter in counters.iter() {
+        if counter.err.is_some() {
+            println!("{}", counter.err.as_ref().unwrap())
+        }
+        for counted_field in settings.counted_fields.iter() {
+            print!("{:width$} ", counter.index(counted_field), width = width)
+        }
+        print!("{}\n", counter.name);
     }
 }
--- word_count/src/main.rs
+++ word_count/src/main.rs
@@ -1,24 +1,71 @@
 mod counter;
-use counter::{Counter, FileCounter};
 
-use clap::Parser;
+use counter::{display_counters, Counter, CounterSettings, FileCounter};
+
+use clap::{AppSettings, ArgAction, Parser};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
+#[clap(global_setting(AppSettings::DeriveDisplayOrder))]
+
 struct Cli {
     #[clap(value_parser, multiple = true, help = "Input files")]
     file: Vec<String>,
+
+    #[clap(short = 'c', long = "bytes", action=ArgAction::Set, help = "Print the byte counts")]
+    count_bytes: Option<bool>,
+
+    #[clap(
+        short = 'm',
+        long = "chars",
+        action=ArgAction::Set,
+        help = "Print the character counts"
+    )]
+    count_chars: Option<bool>,
+
+    #[clap(short = 'w', long = "words", action=ArgAction::Set, help = "Print the word counts")]
+    count_words: Option<bool>,
+
+    #[clap(short = 'l', long = "lines", action=ArgAction::Set, help = "Print the newline counts")]
+    count_lines: Option<bool>,
 }
+
+fn parse_settings(cli: &Cli) -> CounterSettings {
+    let any_specified = [
+        cli.count_bytes,
+        cli.count_chars,
+        cli.count_words,
+        cli.count_lines,
+    ]
+    .iter()
+    .any(|opt| opt.is_some());
+
+    // if any are specified, everything else becomes false
+    // if none are specified, everything is the default value
+
+    if any_specified {
+        CounterSettings::new(
+            cli.count_bytes.unwrap_or(false),
+            cli.count_chars.unwrap_or(false),
+            cli.count_words.unwrap_or(false),
+            cli.count_lines.unwrap_or(false),
+        )
+    } else {
+        CounterSettings::new(true, false, true, true)
+    }
+}
+
 fn main() {
     let cli = Cli::parse();
 
-    let mut totals = Counter::new();
+    let settings = parse_settings(&cli);
+
+    let mut counters: Vec<Counter> = Vec::new();
 
     for file in cli.file {
-        let mut fc = FileCounter::new(file.to_string());
+        let mut fc = FileCounter::new(file.to_string(), settings.clone());
         fc.count().unwrap();
-        totals.add(&fc.counts);
-        println!("{}", fc);
+        counters.push(fc.counts);
     }
-    println!("{} total", totals);
+    display_counters(&mut counters, settings)
 }
